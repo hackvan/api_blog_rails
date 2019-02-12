fefe1ebe1384dc80e05b19087b0f0b43d85accbd
Diego Camacho <hackvan@gmail.com>
2019-02-12 15:41:42 -0500

    Fix test for validate true or false values in :published attribute of Post

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -6,6 +6,7 @@ RSpec.describe Post, type: :model do
       should validate_presence_of(:title)
       should validate_presence_of(:content)
       # should validate_inclusion_of(:published).in_array([true, false])
+      should allow_value(true, false).for(:published)
       should validate_presence_of(:user_id)
     end
   end
