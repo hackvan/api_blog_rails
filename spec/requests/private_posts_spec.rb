9bbedd1c3d607dd547fb7a978a4966c29fb75710
Diego Camacho <hackvan@gmail.com>
2019-02-20 14:03:26 -0500

    Post's show method logic with authorizate users

--- spec/requests/private_posts_spec.rb
+++ spec/requests/private_posts_spec.rb
@@ -18,7 +18,7 @@ RSpec::describe "Posts with authentication", type: :request do
           before { get "/posts/#{other_user_post.id}", headers: auth_headers }
           
           context "payload" do
-            subject { JSON.parse(response.body) }
+            subject { payload }
             it { is_expected.to include(:id) }
           end
           context "response" do
@@ -31,7 +31,7 @@ RSpec::describe "Posts with authentication", type: :request do
           before { get "/posts/#{other_user_draft_post.id}", headers: auth_headers }
           
           context "payload" do
-            subject { JSON.parse(response.body) }
+            subject { payload }
             it { is_expected.to include(:error) }
           end
           context "response" do
@@ -46,12 +46,15 @@ RSpec::describe "Posts with authentication", type: :request do
   end
 
   describe "POST /posts" do
-    it "" do
-    end
   end
 
   describe "PUT /posts/{id}" do
-    it "" do
-    end
   end
+
+  private
+    def payload
+      # esto es para obtener un hash que se pueda 
+      # acceder con "id" o con :id
+      JSON.parse(response.body).with_indifferent_access
+    end
 end
